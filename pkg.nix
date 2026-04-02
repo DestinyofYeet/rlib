6e27f7de81da1b0abbf783b5c846f300148688b6
DestinyofYeet <ole@ole.blue>
2026-04-02 20:57:10 +0200

    feat: compiled mkmerge_wasm

--- pkg.nix
+++ pkg.nix
@@ -1,12 +1,32 @@
-{ rustPlatform, lib, ... }:
+{
+  rustPlatform,
+  lib,
+  pkgs,
+  ...
+}:
 
 rustPlatform.buildRustPackage {
-  pname = "pkg";
+  pname = "rust-nix-lib";
   version = "1.0";
 
   src = ./.;
 
-  cargoHash = "";
+  nativeBuildInputs = with pkgs; [
+    lld
+  ];
+
+  cargoHash = "sha256-gSyWKxnFHJoruAM2K7PEYUjAy9JKOG/hMrdNC4SNy/4=";
+
+  buildPhase = ''
+    cargo build -r --target wasm32-unknown-unknown
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    mkdir $out/lib
+
+    cp target/wasm32-unknown-unknown/release/mkmerge_wasm.wasm $out/lib/mkmerge_wasm.wasm
+  '';
 
   meta = with lib; {
     description = "A program";
