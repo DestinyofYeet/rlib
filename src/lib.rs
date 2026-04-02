34742af3388e41b6400ce25108accd2182485d08
DestinyofYeet <ole@ole.blue>
2026-04-02 14:37:37 +0200

    feat: can merge values

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,39 @@
-use nix_wasm_rust::{Value, warn};
+use std::collections::HashMap;
+
+use nix_wasm_rust::{Type, Value, warn};
 
 #[unsafe(no_mangle)]
-pub extern "C" fn test(arg: Value) -> Value {
-    warn!("Hello, world!");
+pub extern "C" fn mkMerge(arg: Value) -> Value {
+    let list = arg.get_list();
 
-    arg
+    warn!("{list:?}");
+
+    let mut new_values = HashMap::new();
+
+    for elem in list.into_iter() {
+        for (key, value) in elem.get_attrset().into_iter() {
+            match value.get_type() {
+                Type::Int
+                | Type::Float
+                | Type::Bool
+                | Type::String
+                | Type::Path
+                | Type::Null
+                | Type::List
+                | Type::Function => {
+                    new_values.insert(key, value);
+                }
+                Type::Attrs => {
+                    warn!("attrs")
+                }
+            }
+        }
+    }
+
+    let values = new_values
+        .iter()
+        .map(|(key, value)| (key.as_str(), *value))
+        .collect::<Vec<(&str, Value)>>();
+
+    Value::make_attrset(values.as_slice())
 }
