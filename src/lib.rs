fe5b76c410b979dd5ff9c00f14610cc9cb06a62b
DestinyofYeet <ole@ole.blue>
2026-04-02 20:45:02 +0200

    feat: should work

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,23 @@
 use std::collections::HashMap;
 
-use nix_wasm_rust::{Type, Value, warn};
+use nix_wasm_rust::{Type, Value};
 
 #[unsafe(no_mangle)]
 pub extern "C" fn mkMerge(arg: Value) -> Value {
     let list = arg.get_list();
 
-    warn!("{list:?}");
-
-    let mut new_values = HashMap::new();
+    let mut new_values: HashMap<String, Value> = HashMap::new();
 
     for elem in list.into_iter() {
         for (key, value) in elem.get_attrset().into_iter() {
-            match value.get_type() {
-                Type::Int
-                | Type::Float
-                | Type::Bool
-                | Type::String
-                | Type::Path
-                | Type::Null
-                | Type::List
-                | Type::Function => {
+            match new_values.remove(&key) {
+                None => {
                     new_values.insert(key, value);
                 }
-                Type::Attrs => {
-                    warn!("attrs")
+
+                Some(existing) => {
+                    let merged = merge_values(existing, value);
+                    new_values.insert(key, merged);
                 }
             }
         }
@@ -36,4 +29,32 @@ pub extern "C" fn mkMerge(arg: Value) -> Value {
         .collect::<Vec<(&str, Value)>>();
 
     Value::make_attrset(values.as_slice())
+}
+
+fn merge_values(a: Value, b: Value) -> Value {
+    match (a.get_type(), b.get_type()) {
+        (Type::Attrs, Type::Attrs) => {
+            let mut merged = a.get_attrset();
+            for (key, value) in b.get_attrset().into_iter() {
+                match merged.remove(&key) {
+                    None => {
+                        merged.insert(key, value);
+                    }
+                    Some(existing) => {
+                        let merged_new = merge_values(existing, value);
+                        merged.insert(key, merged_new);
+                    }
+                }
+            }
+
+            let values = merged
+                .iter()
+                .map(|(key, value)| (key.as_str(), *value))
+                .collect::<Vec<(&str, Value)>>();
+
+            Value::make_attrset(values.as_slice())
+        }
+
+        (_, _) => b,
+    }
 }
